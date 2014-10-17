51f15ec3549e972b37dd6afbbd162a89653f510f
olgakogan <olgako1@gmail.com>
2014-10-17 15:47:09 +0300

    Initial Commit

---
+++ CopyContacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CopyContacts
+//
+//  Created by Olga Kogan on 10/17/14.
+//  Copyright (c) 2014 Olga Kogan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CopyContactsTests/CopyContactsTests.swift
@@ -0,0 +1,36 @@
+//
+//  CopyContactsTests.swift
+//  CopyContactsTests
+//
+//  Created by Olga Kogan on 10/17/14.
+//  Copyright (c) 2014 Olga Kogan. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CopyContactsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
