d074300b256bcab20994167266326113e297403a
olgakogan <olgako1@gmail.com>
2014-11-02 20:45:59 +0200

    initial version - exports contacts' phones to csv and sends it via mail

---
+++ CopyContacts/Dictionary+Private.swift
@@ -0,0 +1,9 @@
+//
+//  Dictionary+Private.swift
+//  CopyContacts
+//
+//  Created by Olga Kogan on 10/31/14.
+//  Copyright (c) 2014 Olga Kogan. All rights reserved.
+//
+
+import Foundation
---
+++ CopyContacts/Utils.swift
@@ -0,0 +1,9 @@
+//
+//  Utils.swift
+//  CopyContacts
+//
+//  Created by Olga Kogan on 10/18/14.
+//  Copyright (c) 2014 Olga Kogan. All rights reserved.
+//
+
+import Foundation
